641ce3ed97e3d6ca5c6ede4f169f8243be6c9bf2
aivruu <benjaalejandro206@gmail.com>
2025-11-29 02:21:13 -0300

    feat: specify plugin's library-loader and main class

---
+++ plugin/src/main/java/io/github/aivruu/revt/REvtPlugin.java
@@ -0,0 +1,94 @@
+// This file is part of region-events, licensed under the GNU License.
+//
+// Copyright (c) 2024-2025 Alejandro
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+package io.github.aivruu.revt;
+
+import io.github.aivruu.revt.config.application.ConfigurationManager;
+import io.github.aivruu.revt.config.domain.mapped.MainConfigurationModel;
+import io.github.aivruu.revt.config.infrastructure.SimpleConfigurationManager;
+import io.github.aivruu.revt.listener.application.PlayerEventHandler;
+import io.github.aivruu.revt.model.domain.RegionUserRepository;
+import io.github.aivruu.revt.model.infrastructure.RegionUserCacheRepository;
+import io.github.aivruu.revt.service.application.RegionFetchService;
+import io.github.aivruu.revt.service.application.RegionMovementService;
+import io.github.aivruu.revt.service.application.RegionTaskController;
+import io.github.aivruu.revt.service.application.impl.SimpleRegionFetchService;
+import io.github.aivruu.revt.service.application.impl.SimpleRegionMovementService;
+import io.github.aivruu.revt.service.application.impl.SimpleRegionTaskController;
+import io.github.aivruu.revt.util.application.PluginExecutor;
+import io.github.aivruu.revt.util.application.Debugger;
+import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+public class REvtPlugin extends JavaPlugin implements RegionEvents {
+  private final ComponentLogger logger = super.getComponentLogger();
+  private final ConfigurationManager configurationManager = new SimpleConfigurationManager(this.logger, super.getDataPath());
+  private RegionUserRepository regionUserRepository;
+  private RegionFetchService regionFetchService;
+  private RegionMovementService regionMovementService;
+  private RegionTaskController regionTaskController;
+
+  @Override
+  public @NotNull RegionUserRepository regionUserRepository() {
+    if (this.regionUserRepository == null) throw PluginExecutor.NON_INITIALIZED_CACHED_EXCEPTION;
+
+    return this.regionUserRepository;
+  }
+
+  @Override
+  public @NotNull RegionFetchService regionFetchService() {
+    if (this.regionFetchService == null) throw PluginExecutor.NON_INITIALIZED_CACHED_EXCEPTION;
+
+    return this.regionFetchService;
+  }
+
+  @Override
+  public @NotNull RegionMovementService regionMovementService() {
+    if (this.regionMovementService == null) throw PluginExecutor.NON_INITIALIZED_CACHED_EXCEPTION;
+
+    return this.regionMovementService;
+  }
+
+  @Override
+  public void onLoad() {
+    if (!this.configurationManager.load()) throw new IllegalStateException("Failed to load the configuration-file, check by possible invalid syntax-errors.");
+
+    final MainConfigurationModel config = this.configurationManager.config();
+    Debugger.enable(config.debug);
+    PluginExecutor.buildExecutor(config.threadPoolSize, config.threadPoolName);
+
+    this.regionUserRepository = new RegionUserCacheRepository();
+    this.regionFetchService = new SimpleRegionFetchService();
+    this.regionMovementService = new SimpleRegionMovementService(this.regionUserRepository, this.regionFetchService, this);
+
+    this.regionTaskController = new SimpleRegionTaskController(this.regionMovementService, this.configurationManager);
+  }
+
+  @Override
+  public void onEnable() {
+    super.getServer().getPluginManager().registerEvents(new PlayerEventHandler(this.regionUserRepository, this.regionMovementService), this);
+
+    this.regionTaskController.start();
+    RegionEventsProvider.set(this);
+  }
+
+  @Override
+  public void onDisable() {
+    this.regionTaskController.stop();
+    this.regionUserRepository.clearSync();
+  }
+}
--- plugin/src/main/java/io/github/aivruu/revt/library/application/PluginLibraryLoader.java
+++ plugin/src/main/java/io/github/aivruu/revt/library/application/PluginLibraryLoader.java
@@ -1,6 +1,6 @@
-// This file is part of regions, licensed under the GNU License.
+// This file is part of region-events, licensed under the GNU License.
 //
-// Copyright (c) 2024 aivruu
+// Copyright (c) 2024-2025 Alejandro
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -14,22 +14,25 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
-package io.github.aivruu.regionevents.library;
+package io.github.aivruu.revt.library.application;
 
+import io.github.aivruu.revt.Constants;
 import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
 import io.papermc.paper.plugin.loader.PluginLoader;
 import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.repository.RemoteRepository;
+import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("UnstableApiUsage")
-public final class RegionEventsPluginLoader implements PluginLoader {
+public final class PluginLibraryLoader implements PluginLoader {
   @Override
-  public void classloader(final PluginClasspathBuilder classpathBuilder) {
-    final var libraryResolver = new MavenLibraryResolver();
+  public void classloader(final @NotNull PluginClasspathBuilder classpathBuilder) {
+    final MavenLibraryResolver libraryResolver = new MavenLibraryResolver();
     libraryResolver.addRepository(new RemoteRepository.Builder("central", "default", "https://repo1.maven.org/maven2/").build());
-    libraryResolver.addDependency(new Dependency(new DefaultArtifact("org.spongepowered:configurate-hocon:4.1.2"), null));
+
+    libraryResolver.addDependency(new Dependency(new DefaultArtifact("org.spongepowered:configurate-yaml:" + Constants.CONFIGURATE_VERSION), null));
     classpathBuilder.addLibrary(libraryResolver);
   }
 }
