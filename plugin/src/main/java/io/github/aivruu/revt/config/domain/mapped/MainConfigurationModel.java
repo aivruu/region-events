6ea154c65e82454293bd477daaa8a07e847959d9
aivruu <benjaalejandro206@gmail.com>
2026-01-01 03:14:40 -0300

    feat(config): include more options and some from previous-versions

--- plugin/src/main/java/io/github/aivruu/revt/config/domain/mapped/MainConfigurationModel.java
+++ plugin/src/main/java/io/github/aivruu/revt/config/domain/mapped/MainConfigurationModel.java
@@ -16,6 +16,7 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 package io.github.aivruu.revt.config.domain.mapped;
 
+import io.github.aivruu.revt.RegionTrackingType;
 import io.github.aivruu.revt.config.domain.ConfigurationContract;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 import org.spongepowered.configurate.objectmapping.meta.Comment;
@@ -32,16 +33,33 @@ public final class MainConfigurationModel implements ConfigurationContract {
 
   @Comment("""
      Whether enable or not the debug-mode.
-     
      This option enables a more-verbose logging for the plugin, useful for debugging purposes and troubleshooting.""")
   public boolean debug = true;
 
   @Comment("""
+     The tracking-type to be performed to handle region-movement events.
+     SINGLE_WORLD: The plugin will just track the world specified by the 'tracked-world' option.
+     MULTI_WORLD: The plugin will track all the worlds specified in the 'for-tracking-worlds' option.
+     GLOBAL: The plugin will track the movement for all the players connected to the server, regardless of the world they're in.""")
+  public RegionTrackingType tracking = RegionTrackingType.SINGLE_WORLD;
+
+  @Comment("The world to be tracked for region-movement events.")
+  public String trackedWorld = "world";
+
+  @Comment("Worlds that won't be tracked for region-movement events, this option is used only if 'track-single-world' is turned off.")
+  public String[] forTrackingWorlds = {
+     "world",
+     "world_nether",
+  };
+
+  @Comment("""
     The update-rate to set for the async-task.
 
-    The plugin uses an async-task to check and monitor movements for any region event-firing instead using a listener
-    for player's movements, this for a plugin's performance and avoid possible server lag-spikes if there are too many
-    players.""")
+    Normally you'd want to keep this value as low as possible so the tracking for region-events is faster, but consequently it may
+    affect the server's performance if there're many players distributed across multiple-worlds.
+    
+    If you want to keep a good tracking time without affecting the server's performance too much, consider set this value to 2 or 3,
+    you can play with the values until you find a good balance. If you're using the 'track-single-world' option this advice can be ignored.""")
   public byte taskUpdateRate = 1;
 
   @Comment("The time-unit to used for the update-rate.")
